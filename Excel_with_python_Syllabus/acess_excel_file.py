752669d595f909124bf9198106ca7082e194888a
Ajay Kumar Vishwakarma <97731696+Ajay50171711@users.noreply.github.com>
2026-07-03 00:11:07 +0530

    Notes for module 1
    pip install pandas openpyxl xlsxwriter xlwings matplotlib numpy
    
    code 1:
    Exercise 1:
    List all Excel files in a folder.
    
    code 2:
    Exercise 2:
    Using pathlib.
    
    code 3:
    handle corrupt file
    
    code 4:
    Fully manipulate Excel files
    Automate formatting
    
    code 5:
    import file using panda
    
    code 6:
    Filtering:
    
    code 7:
    Grouping:

---
+++ Excel_with_python_Syllabus/acess_excel_file.py
@@ -0,0 +1,66 @@
+# method 1: using os
+# import os
+
+# folder = "reports"
+
+# files = os.listdir(folder)
+
+# for file in files:
+#     if file.endswith(".xlsx"):
+#         print(file)
+
+
+# method 2: using pathlib
+
+# from pathlib import Path
+
+# folder = Path("reports")
+
+# for file in folder.glob("*.xlsx"):
+#     print(file)
+
+
+# def process_file(filename):
+#     try:
+#         print(f"Processing {filename}")
+#     except Exception as e:
+#         print("Error:", e)
+
+# process_file("sales.xlsx")
+
+
+
+
+# from openpyxl import Workbook
+
+# wb = Workbook()
+# ws = wb.active
+
+# ws['A1'] = "Name"
+# ws['B1'] = "Salary"
+
+# ws['A2'] = "John"
+# ws['B2'] = 50000
+
+# wb.save("employee.xlsx")
+
+
+import pandas as pd
+import os
+
+file_path = "reports/sales_feb.xlsx"
+
+if os.path.exists(file_path):
+    df = pd.read_excel(file_path)
+    print(df.head())
+    print("\n\n")
+else:
+    print("File not found:", file_path)
+
+high_sales = df[df["Amount_INR"] > 10000]
+
+print(high_sales)
+
+# df.groupby("Region")["Sales"].sum()  #apne data me naahi region ha na hi multiple city hai show that we have to generate a data which contain reqire data
+
+
